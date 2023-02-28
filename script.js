40662b80625420c4e285bc0b54c8779ac76c48e1
Mateusz Krochmal <81087684+KroMateusz@users.noreply.github.com>
2023-02-28 21:18:17 +0100

    Add files via upload
    Finished HTML and CSS

---
+++ script.js
@@ -0,0 +1,27 @@
+const expirationSelect = document.querySelector('[data-expiration-year]');
+
+const currentYear = new Date().getFullYear();
+for (let i = currentYear; i < currentYear + 10; i++) {
+	const option = document.createElement('option');
+	option.value = i;
+	option.innerText = i;
+	expirationSelect.append(option);
+}
+
+document.addEventListener('keydown', (e) => {
+	const input = e.target;
+	const key = e.key;
+	if (!isConnectedInput(input)) return;
+
+	switch (key) {
+		case 'ArrowLeft': {
+		}
+		default: {
+		}
+	}
+});
+
+function isConnectedInput(input) {
+	const parent = input.closest('[data-connected-inputs]');
+	return target.matches('input') && parent != null;
+}
